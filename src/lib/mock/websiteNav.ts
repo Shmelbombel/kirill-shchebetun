db3461769c539dc15e746960710d7d1a24d331f0
Shmelbombel <123874012+Shmelbombel@users.noreply.github.com>
2025-02-27 22:26:53 +0500

    Update websiteNav.ts

--- src/lib/mock/websiteNav.ts
+++ src/lib/mock/websiteNav.ts
@@ -1,7 +1,6 @@
 export enum WEBSITE_NAV {
   ABOUT = "About",
   CONTACTS = "Contacts",
-  LOCATION = "Location",
 }
 
 export const websiteNavObj: {
@@ -9,11 +8,9 @@ export const websiteNavObj: {
 } = {
   [WEBSITE_NAV.ABOUT]: { title: "Обо мне", href: "#about" },
   [WEBSITE_NAV.CONTACTS]: { title: "Обратная связь", href: "#contacts" },
-  [WEBSITE_NAV.LOCATION]: { title: "Адрес", href: "#address" },
 };
 
 export const websiteNavArr = [
   websiteNavObj.About,
   websiteNavObj.Contacts,
-  websiteNavObj.Location,
 ];
